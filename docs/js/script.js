307da3130f0f190c31fd254b5ebfbe53241ee071
Henry Yip <hya59@sfu.ca>
2021-01-06 22:58:15 -0800

    more css

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,17 +1,45 @@
 
-// Animation handling
+//-- Animation handling --//
+
+//id card fanout
 var card = document.getElementsByClassName('id-card');
+
 card[0].addEventListener('animationend', function(e) {
-    card[0].classList.remove('animated');
+  card[0].classList.remove('animated');
 });
+
 card[0].addEventListener('mouseover', function(e) {
-    card[0].classList.add('animated')
+  card[0].classList.add('animated')
 })
 
+// see-more arrow bounce
 var arrow = document.getElementsByClassName('arrow down');
+
 arrow[0].addEventListener('animationend', function(e) {
-    arrow[0].classList.remove('animated');
+  arrow[0].classList.remove('animated');
 });
+
 arrow[0].addEventListener('mouseover', function(e) {
-    arrow[0].classList.add('animated')
+  arrow[0].classList.add('animated')
 })
+
+// progress bar on scroll into view
+
+var $section = $('#portfolio');
+
+function animateBars() {
+  $(".progress-bar-fill").each(function(){
+    $(this).removeClass("animated");
+  });
+}
+
+$(document).bind('scroll', function(e) {
+  var scrollOffset = $(document).scrollTop();
+  var containerOffset = $section.offset().top - window.innerHeight;
+  if (scrollOffset > containerOffset) {
+    setTimeout(function(){
+      animateBars();
+    },500);
+    $(document).unbind('scroll');
+  }
+});
